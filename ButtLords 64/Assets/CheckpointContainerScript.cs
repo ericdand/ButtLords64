91db24f73a1e0f44a3feeba0678a26f957ff8c24
Eric Dand <eric.c.dand@gmail.com>
2016-02-26 21:33:58 -0500

    Add the beginnings of a checkpoint-based movement system, physics objects for player character.

---
+++ ButtLords 64/Assets/CheckpointContainerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointContainerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Expose all child checkpoints to other scripts.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
