3770cf63384a06555f280d7f1f08780c0c84fe5d
Eric Dand <eric.c.dand@gmail.com>
2016-06-10 06:30:15 -0500

    Make waypoints notice when they are visited.
    Also, try playing this version… WHEEEEEEE!!!!

--- ButtLords 64/Assets/Waypoint.cs
+++ ButtLords 64/Assets/Waypoint.cs
@@ -3,15 +3,33 @@ using System.Collections;
 
 public class Waypoint : MonoBehaviour {
 
+	public Transform player;
+
 	internal bool isVisited;
 
 	// Use this for initialization
 	void Start () {
+		if (player == null) {
+			Debug.LogWarning ("Player object of waypoint not set");
+		}
 	}
-	
+
+	private bool withinEpsilon (Vector3 a, Vector3 b, float e) {
+		Debug.Log("dx: " + (a.x - b.x) + "\ndy: " + (a.y - b.y) + "\ndz: " + (a.z + b.z));
+		return (Mathf.Abs (a.x - b.x) <= e
+		&& Mathf.Abs (a.y - b.y) <= e
+		&& Mathf.Abs (a.z - b.z) <= e);
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (player) {
+			var playerWidth = player.GetComponent<BoxCollider2D> ().size.x;
+			if (withinEpsilon (player.position, this.transform.position, playerWidth / 2)) {
+				isVisited = true;
+				Debug.Log ("I AM VISITED!");
+			}
+		}
 	}
 
 	public Vector2 Position() {
