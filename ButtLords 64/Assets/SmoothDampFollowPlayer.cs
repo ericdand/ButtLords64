fc5a06953a3f1b4c49e3c3a8bca67fa3ea44beaa
Eric Dand <eric.c.dand@gmail.com>
2016-06-10 03:19:23 -0700

    Add SmoothDampFollowPlayer (eew) script
    Used to make the camera follow the player in a smooth, damp fashion.

---
+++ ButtLords 64/Assets/SmoothDampFollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothDampFollowPlayer : MonoBehaviour {
+
+	public Transform target;
+
+	private Vector2 velocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.SmoothDamp (transform.position, target.position, ref velocity, 0.15f);
+	}
+}
