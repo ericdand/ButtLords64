d2a431aa66ad1c08e034dabe8eae04793d0b881a
Eric Dand <eric.c.dand@gmail.com>
2016-06-11 02:14:24 -0500

    Visit waypoints in order.

--- ButtLords 64/Assets/PlayerScript.cs
+++ ButtLords 64/Assets/PlayerScript.cs
@@ -6,23 +6,35 @@ public class PlayerScript : MonoBehaviour {
 	// Public so it can be set from the Unity editor.
 	public WaypointContainerScript waypointContainer;
 	Rigidbody2D rb2d;
-	float speed = 1f;
+	float speed = 0.1f;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
-	
+		
+	private bool withinEpsilon (Vector3 a, Vector3 b, float e) {
+		// Debug.Log("dx: " + (a.x - b.x) + "\ndy: " + (a.y - b.y) + "\ndz: " + (a.z + b.z));
+		return (Mathf.Abs (a.x - b.x) <= e
+			&& Mathf.Abs (a.y - b.y) <= e
+			&& Mathf.Abs (a.z - b.z) <= e);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		// Go to the next waypoint.
 		var waypoint = waypointContainer.GetNextWaypoint();
-		if (waypoint == null)
-			return; // TODO
-		var direction = (waypoint.Position () - this.rb2d.position);
-		if (direction.magnitude > speed) {
-			direction = direction.normalized * speed;
+		if (waypoint != null) {
+			var width = this.GetComponent<BoxCollider2D> ().size.x;
+			if (withinEpsilon (waypoint.transform.position, this.transform.position, width / 2)) {
+				waypoint.isVisited = true;
+				Debug.Log ("I AM VISITED!");
+			}
+			var direction = (waypoint.Position () - this.rb2d.position);
+			if (direction.magnitude > speed) {
+				direction = direction.normalized * speed;
+			}
+			rb2d.MovePosition (this.rb2d.position + direction);
 		}
-		rb2d.MovePosition(this.rb2d.position + direction);
 	}
 }
